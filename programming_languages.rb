c6857e9a60ceb6eb882266d4d65ee9a4576e7f07
Fillan Lee <fillanlee@aol.com>
2019-02-17 14:46:59 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,12 +2,14 @@ def reformat_languages(languages)
   # your code here
   new_lang = {}
   languages.each do |styple, language|
+    
     language.each do |name, attri|
-      attri.each do |type, value|
+      
+      attri.each do |k, v|
         
       
       if new_lang.key?(name) == false
-      new_lang[name] = {type => value, :style => [styple]}
+      new_lang[name] = {k => v, :style => [styple]}
     else
       new_lang[name][:style] << styple
     end
