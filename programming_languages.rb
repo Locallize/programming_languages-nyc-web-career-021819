fb59b280f07f49ab180f92cb9dcfed9fa1c17448
Fillan Lee <fillanlee@aol.com>
2019-02-17 14:52:02 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,12 +6,14 @@ def reformat_languages(languages)
     language.each do |name, attri|
       
       attri.each do |k, v|
-        
+ #now I have all the key value pairs, add them to the new hash       
       
       if new_lang.key?(name) == false
       new_lang[name] = {k => v, :style => [styple]}
+      #create languages first
     else
       new_lang[name][:style] << styple
+      #only add new style key
     end
   end
     end
