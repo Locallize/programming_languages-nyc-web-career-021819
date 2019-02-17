10d097168b6ffec4e1c378e5fd55803ab95c4bba
Fillan Lee <fillanlee@aol.com>
2019-02-17 14:16:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,9 @@ def reformat_languages(languages)
   languages.each do |styple, languages|
     languages.each do |name, attri|
       
-      new_lang
+      new_lang[name] ||= attri
+      new_lang[name][:style] = []
+      new_lang[name][:style]
     end
   end
 end
